79478e9311aeec7bddb98f94811861c91dcd0f8c
pdotsani <pat.sanjuan@gmail.com>
2024-03-05 18:28:02 -0800

    add spotify official library, made playlist request

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
-const express = require('express');
 require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser'); 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const SpotifyWebApi = require('spotify-web-api-node');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri: process.env.SPOTIFY_REDIRECT_URI
-});
+const { SpotifyApi } = require("@spotify/web-api-ts-sdk");
 
-function getClientCredentials() {
-  spotifyApi.clientCredentialsGrant().then((data) => {
-    console.log("access: ", data.body)
-  });
-}
+const api = SpotifyApi.withClientCredentials(
+  process.env.SPOTIFY_CLIENT_ID,
+  process.env.SPOTIFY_CLIENT_SECRET
+);
+
+let accessToken;
 
 app.listen(port, () => {
   console.log(`Tampulan api listening on port ${port}`)
-  getClientCredentials();
+
+  api.authenticate().then((data) => {
+    console.log(data);
+    accessToken = data.accessToken.access_token;
+    fetch('https://api.spotify.com/v1/browse/categories/focus?locale=en', {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    }).then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+      fetch(`https://api.spotify.com/v1/browse/categories/${data.id}/playlists`, {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
+        }
+      }).then((response) => {
+        return response.json();
+      }).then((data) => {
+        console.log(data);
+      });
+    })
+  });
+
 })
