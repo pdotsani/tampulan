988c008560ede4939c44a1dfc3737ff25730fff4
pdotsani <pat.sanjuan@gmail.com>
2024-03-14 13:08:33 -0700

    refactor spotify service

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import dotenv from 'dotenv';
 import logger from './logger';
-import { SpotifyApi } from '@spotify/web-api-ts-sdk';
 import { chooseRamdomPlaylist, createPayload, shuffleTracks } from './utils';
-import { getAccessToken, getCategory, getPlaylists, getTracks } from './spotify';
+import Spotify from './spotify';
 import routes from './config/routes';
 import auth from './auth';
 
@@ -18,19 +17,15 @@ const log = logger.init();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const api = SpotifyApi.withClientCredentials(
-  process.env.SPOTIFY_CLIENT_ID,
-  process.env.SPOTIFY_CLIENT_SECRET
-);
+const spotify = new Spotify();
 
 app.get(routes.GET_TRACKS, auth, async (req, res) => {
   log.info(`${req.method} - ${req.originalUrl} - ${req.headers['x-forwarded-for'] || req.socket.remoteAddress} - ${req.get('User-Agent')}`);
 
-  const token = await getAccessToken(api);
-  const category = await getCategory(token);
-  const playlists = await getPlaylists(token, category.id);
+  const category = await spotify.getCategory();
+  const playlists = await spotify.getPlaylists(category.id);
   const playlist = await chooseRamdomPlaylist(playlists.playlists);
-  const tracks = await getTracks(token, playlist.id);
+  const tracks = await spotify.getTracks(playlist.id);
   const shuffledTracks = shuffleTracks(tracks);
 
   log.info(`Returning ${shuffledTracks.length} tracks`);
@@ -38,6 +33,8 @@ app.get(routes.GET_TRACKS, auth, async (req, res) => {
   res.send(createPayload(shuffledTracks));
 });
 
-app.listen(PORT, () => {
-  console.log(`Tampulan api listening on port ${PORT}`);
+app.listen(PORT, async () => {
+  log.info(`Tampulan api listening on port ${PORT}`);
+  await spotify.getAccessToken();
+  log.info('Spotify Access token retrieved');
 });
