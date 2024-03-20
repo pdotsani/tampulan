e0af477a8522cb759f6962047c2eeabed608cd94
pdotsani <pat.sanjuan@gmail.com>
2024-03-20 13:58:20 -0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import logger from './logger';
 import routes from './config/routes';
 import router from './routes';
@@ -15,6 +16,10 @@ const log = logger.init();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors());
+};
 
 app.listen(PORT, async () => {
   log.info(`Tampulan api listening on port ${PORT}`);
